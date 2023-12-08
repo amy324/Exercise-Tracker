20914af8340baf48010645f3bddcde512b939104
amy-monroe <amy.monroe97@gmail.com>
2023-12-08 16:28:59 +0000

    updates

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
 const users = [];
 
 
 function generateUserId() {
   return Math.random().toString(36).substr(2, 9); 
 }
+
 
 app.post('/api/users', (req, res) => {
   const { username } = req.body;
@@ -27,34 +29,29 @@ app.post('/api/users', (req, res) => {
     return res.json({ error: 'Username already taken' });
   }
 
-
   const newUser = {
     username,
     _id: generateUserId(),
   };
 
-
   users.push(newUser);
 
-  res.json({ username: newUser.username, _id: newUser._id }); 
+  res.json(newUser); 
 });
 
 app.get('/api/users', (req, res) => {
   res.json(users.map(user => ({ username: user.username, _id: user._id })));
 });
 
-// Add exercises for a user
 app.post('/api/users/:_id/exercises', (req, res) => {
   const { _id } = req.params;
   const { description, duration, date } = req.body;
 
   const user = users.find(user => user._id === _id);
 
-
   if (!user) {
     return res.json({ error: 'User not found' });
   }
-
 
   const newExercise = {
     description,
@@ -62,20 +59,17 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     date: date || new Date().toDateString(),
   };
 
- 
   if (!user.log) {
     user.log = [];
   }
   user.log.push(newExercise);
   user.count = user.log.length;
 
-  res.json(user);
+  res.json(user); 
 });
-
 
 app.get('/api/users/:_id/logs', (req, res) => {
   const { _id } = req.params;
-
 
   const user = users.find(user => user._id === _id);
 
@@ -83,7 +77,12 @@ app.get('/api/users/:_id/logs', (req, res) => {
     return res.json({ error: 'User not found' });
   }
 
-  res.json(user);
+  res.json({
+    _id: user._id,
+    username: user.username,
+    count: user.count || 0,
+    log: user.log || [],
+  }); 
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
