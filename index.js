c0e02a1306e032787df26148dd2944d295bdaa1d
amy-monroe <amy.monroe97@gmail.com>
2023-12-08 16:24:55 +0000

    response modifications

--- index.js
+++ index.js
@@ -12,56 +12,57 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// defines the users array to store user data
 const users = [];
 
-// helper function to generate a unique user ID
+
 function generateUserId() {
   return Math.random().toString(36).substr(2, 9); 
 }
 
-// creates a new user
 app.post('/api/users', (req, res) => {
   const { username } = req.body;
 
-  // checks if the username already exists
+
   if (users.some(user => user.username === username)) {
     return res.json({ error: 'Username already taken' });
   }
 
-  // creates a new user
+
   const newUser = {
     username,
     _id: generateUserId(),
   };
 
-  // adds the user to the array
+
   users.push(newUser);
 
-  res.json(newUser);
+  res.json({ username: newUser.username, _id: newUser._id }); 
 });
 
-// adds user exercises 
+app.get('/api/users', (req, res) => {
+  res.json(users.map(user => ({ username: user.username, _id: user._id })));
+});
+
+// Add exercises for a user
 app.post('/api/users/:_id/exercises', (req, res) => {
   const { _id } = req.params;
   const { description, duration, date } = req.body;
 
-  // finds the user by ID
   const user = users.find(user => user._id === _id);
 
-  // checks if the user exists
+
   if (!user) {
     return res.json({ error: 'User not found' });
   }
 
-  // creates a new exercise
+
   const newExercise = {
     description,
     duration: parseInt(duration),
     date: date || new Date().toDateString(),
   };
 
-  // adds the exercise to the user's log
+ 
   if (!user.log) {
     user.log = [];
   }
@@ -71,14 +72,13 @@ app.post('/api/users/:_id/exercises', (req, res) => {
   res.json(user);
 });
 
-// gets a user's exercise log
+
 app.get('/api/users/:_id/logs', (req, res) => {
   const { _id } = req.params;
 
-  // finds the user by ID
+
   const user = users.find(user => user._id === _id);
 
-  // checks if the user exists
   if (!user) {
     return res.json({ error: 'User not found' });
   }
